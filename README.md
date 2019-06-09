9b250a9a34c80ed47f8c08153a8c62c05a33ff35
dyang504 <dengyang92@qq.com>
2019-06-09 23:39:08 +0800

    init the repo

---
+++ README.md
@@ -0,0 +1,8 @@
+This project is a simple showcase of personal workouts data based on the Python dash library. The project contains there layers: MVC. In the model layer, sqlite is used for storing data, SQL is used for query and reshape the data to meet the graphs' needs. In the View and Controller layer, dash a python library build on top of the Flask and plotly.js is used for handle all the details.
+
+![Sreen Shot](Screen_Shot.png)
+
+TODO
+  Complete the meter
+  Add interactions
+  
---
+++ database_helpler.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# df = pd.read_csv('strong.csv')
+# df['id'] = df['日期'].astype(str) + df['运动名称'].astype(str) + df['设置顺序'].astype(str)
+# df['id'] = df['id'].replace('[\s+|\-|(|)|:]', '', regex=True)
+
+def fetch_data(q,conn):
+    return pd.read_sql(sql=q,con=conn)  
+
+conn = create_engine('sqlite:///./train_items.db')
+# df.to_sql('train_record',conn)
+
+
+def query_exist_data_in_db(conn):
+    q = 'SELECT * FROM train_record'
+    return fetch_data(q,conn)
+
+
+
+def handle_new_data(path,conn):
+    exist_data = query_exist_data_in_db(conn)
+    del exist_data['index']
+    new_data = pd.read_csv(path)
+    new_data['id'] = new_data['日期'].astype(str) + new_data['运动名称'].astype(str) + new_data['设置顺序'].astype(str)
+    new_data['id'] = new_data['id'].replace('[\s+|\-|(|)|:]', '', regex=True)  
+    diff_data = pd.concat([exist_data,new_data]).drop_duplicates(keep=False)
+    return diff_data 
+
+# handle_new_data('strong_copy.csv',conn).to_csv('diff.csv',index=None)
+
+def update_data(path,conn):
+    df = pd.read_csv(path)
+    df.to_sql('train_record',conn,if_exists='append')
+
+update_data('diff.csv',conn)
